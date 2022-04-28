86d0f0598088dc0b3dd0aa6f2b6738a7b6aaed22
JSM788 <salinasjuan788@gmail.com>
2022-04-28 12:38:24 -0500

    Write a program that does mathematical calculations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    add = add(a, b)
+    sub = sub(a, b)
+    mul = mul(a, b)
+    div = div(a, b)
+    print(f"{a} + {b} = {add}")
+    print(f"{a} + {b} = {sub}")
+    print(f"{a} + {b} = {mul}")
+    print(f"{a} + {b} = {div}")
