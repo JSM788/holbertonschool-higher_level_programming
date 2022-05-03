098d8cdf00f675b208d5392884f8228eb0ebcf61
JSM788 <salinasjuan788@gmail.com>
2022-05-02 21:05:12 -0500

    Try4

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 
 def multiple_returns(sentence):
     lens = len(sentence)
-    if lens is None:
+    if sentence is None:
         return(None, sentence[0])
     else:
         return(lens, sentence[0])
