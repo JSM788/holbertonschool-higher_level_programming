ec1b725856e4415be456276efae411cba628f8e2
JSM788 <salinasjuan788@gmail.com>
2022-05-04 20:40:33 -0500

    Fix condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary is not None:
+    if not a_dictionary:
+        return (None)
+    else:
         biggest_key = max(a_dictionary, key=a_dictionary.get)
         return(biggest_key)
-    else:
-        return(None)
