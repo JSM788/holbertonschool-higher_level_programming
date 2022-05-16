e055a998829c15f15d888ffbda6bc1c0a707d611
JSM788 <salinasjuan788@gmail.com>
2022-05-16 14:11:37 -0500

    Maybe now, write a function that prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 
 def safe_print_integer(value):
     try:
-        if value > 0 or value <= 0:
-            print("{:d}".format(value))
-            return True
-    except TypeError:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
         return False
