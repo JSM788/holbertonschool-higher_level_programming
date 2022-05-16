d1b18c86ea823ec36c6cbcd63e2514312489d50e
JSM788 <salinasjuan788@gmail.com>
2022-05-16 12:11:07 -0500

    Try3

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,10 @@
 def safe_print_list(my_list=[], x=0):
     count = 0
     for i in my_list[0:x]:
+        try:
             print(i, end="")
             count += 1
+        except ValueError:
+            print("Error")
     print()
     return count
