73ed18d5ebdb237ffb7083b3e904dbe5a83e8858
JSM788 <salinasjuan788@gmail.com>
2022-05-16 19:02:46 -0500

    Fixing position of count

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(0, x):
         try:
-            count += 1
             print("{:d}".format(my_list[i]), end="")
-        except(ValueError, TypeError):
+            count += 1
+        except (ValueError, TypeError):
             pass
     print()
     return count
