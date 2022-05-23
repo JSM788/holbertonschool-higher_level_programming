185b2ffe9dd9df5ff173551beee7d2bb6f25cfe1
JSM788 <salinasjuan788@gmail.com>
2022-05-23 18:28:03 -0500

    Write a class rectangle and add its height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Creating a class Rectangle"""
+
+
+class Rectangle:
+    """Initializing the values """
+
+    def __init__(self, width=0, height=0):
+
+        self.__height = height
+        self.__width = width
+
+    """Get height values"""
+    @property
+    def height(self):
+        return(self.__height)
+
+    """Update height values"""
+    @height.setter
+    def height(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    """Get width values"""
+    @property
+    def width(self):
+        return(self.__width)
+
+    """Update height values"""
+    @width.setter
+    def width(self, value):
+        if isinstance(value, int) is not True:
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
