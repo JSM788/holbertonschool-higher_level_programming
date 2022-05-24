4c22bedd589b5b0ebf02253406ae1aa8e210f4e1
JSM788 <salinasjuan788@gmail.com>
2022-05-23 19:03:04 -0500

    removing the underscores

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -6,9 +6,8 @@ class Rectangle:
     """Initializing the values """
 
     def __init__(self, width=0, height=0):
-
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     """Get width values"""
     @property
