ff2fb8221e1b744996460a281429c1047cb31b72
JSM788 <salinasjuan788@gmail.com>
2022-05-30 18:57:19 -0500

    Write a function that returns attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""function that returns the attributes and methods of an object"""
+
+
+def lookup(obj):
+    """returns the properties and methods of an object"""
+    return dir(obj)
