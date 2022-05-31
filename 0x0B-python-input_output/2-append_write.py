474cbc9ce6aadcb26f2601ffab14b7e4bfd4da17
JSM788 <salinasjuan788@gmail.com>
2022-05-31 18:53:20 -0500

    Add flag a

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Function that appends a string at the end of a text/
-file"""
+"""Append to a file"""
 
 
 def append_write(filename="", text=""):
-    """Returns the number of characters added"""
-
-    with open(filename, "w", encoding="utf-8") as f:
-        write_data = f.write(text)
-        write_data = f.write(text)
-        return(write_data)
+    """Appends a string at the end of a text file (UTF8)
+    and returns the number of characters added"""
+    with open(filename, "a", encoding="utf-8") as fo:
+        return fo.write(text)
