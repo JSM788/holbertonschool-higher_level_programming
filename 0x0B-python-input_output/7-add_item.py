de76069de9bf19ac4d3ab387c4da092f75a96000
JSM788 <salinasjuan788@gmail.com>
2022-05-31 23:24:59 -0500

    Add finally

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,13 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    lista = []
-    for i in range(1, len(sys.argv)):
-        lista.append(sys.argv[i])
-    new_lista = load_from_json_file('add_item.json')
-    new_lista += lista
-    save_to_json_file(new_lista, 'add_item.json')
-
+    new_list = load_from_json_file('add_item.json')
 except FileNotFoundError:
     save_to_json_file([], 'add_item.json')
+finally:
+    lis = []
+    for i in range(1, len(sys.argv)):
+        lis.append(sys.argv[i])
+    new_list = load_from_json_file('add_item.json')
+    new_list += lis
+    save_to_json_file(new_list, 'add_item.json')
