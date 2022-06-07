59d0caf2da57720eb380ec5d9b6f2d4b5d1c2669
JSM788 <salinasjuan788@gmail.com>
2022-06-06 21:20:19 -0500

    Add comments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,7 +9,7 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-
+        """Initializing constructor method"""
         if id is not None:
             self.id = id
         else:
