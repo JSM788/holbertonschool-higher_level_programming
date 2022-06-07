e5a33c9e6308981c4e4d5572fd6a17199d00b721
JSM788 <salinasjuan788@gmail.com>
2022-06-06 20:56:43 -0500

    Creating the base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,41 @@
+#!/usr/bin/pythonear
+"""Defining id for all other classes"""
+import json
+
+
+class Base():
+    """Defines a Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        if list_objs is None:
+            return []
+        with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
+
+            new_list = []
+            for i in list_objs:
+                dictionary = i.to_dictionary()
+                new_list.append(dictionary)
+            return f.write(Base.to_json_string(new_list))
+
+    def from_json_string(json_string):
+        if json_string is None:
+            return []
+        else:
+            return json.loads(json_string)
