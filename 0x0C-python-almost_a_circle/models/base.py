3feb52308fc28913ceccda1d4e423ac35720f146
JSM788 <salinasjuan788@gmail.com>
2022-06-06 21:27:36 -0500

    Add comments2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,12 +18,16 @@ class Base():
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """Returns the JSON string representation
+        of list_dictionaries"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
+        """writes the JSON string representation
+        of list_objs to a file"""
         if list_objs is None:
             return []
         with open(cls.__name__ + ".json", "w", encoding="utf-8") as f:
@@ -35,6 +39,8 @@ class Base():
             return f.write(Base.to_json_string(new_list))
 
     def from_json_string(json_string):
+        """Returns the list of the JSON string
+        representation json_string"""
         if json_string is None:
             return []
         else:
