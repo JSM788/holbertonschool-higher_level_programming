da2dcce647f24c0c8e163c54f98914ed2e31b77f
JSM788 <salinasjuan788@gmail.com>
2022-08-03 13:10:23 -0500

    Reordering the code

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -3,11 +3,11 @@
 if (Number(process.argv[2])) {
   console.log(factorial(process.argv[2]));
 } else {
-  console.log('1');
+  console.log(1);
 }
 
 function factorial (n) {
-  if (!n || n < 2) {
+  if (n === 0) {
     return 1;
   }
   return (n * factorial(n - 1));
