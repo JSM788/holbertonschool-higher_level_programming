fa82ae697fceff0a2081dcdb30598ea2716d2af3
Juan Salinas <salinasjuan788@gmail.com>
2022-08-06 21:29:06 -0700

    Adding states.id

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,16 +5,15 @@ import sys
 
 if __name__ == '__main__': 
 
-        db = MySQLdb.connect(host='localhost',
+    db = MySQLdb.connect(host='localhost',
                         port=3306,
                         user=sys.argv[1],
                         passwd=sys.argv[2],
                         db=sys.argv[3])
 
-        cur = db.cursor()
-        cur.execute('SELECT * FROM states ORDER BY id ASC')
-        rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute('SELECT * FROM states ORDER BY states.id ASC')
+    rows = cur.fetchall()
 
-        for row in rows:
-            print(row)
-
+    for row in rows:
+        print(row)
