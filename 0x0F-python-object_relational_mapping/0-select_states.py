1c82de2644b10cfc8c31552c9f268c7f56149e40
Juan Salinas <salinasjuan788@gmail.com>
2022-08-06 21:34:27 -0700

    Adding from sys

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """List all states from the database hbtn_0e_0_usa""" 
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == '__main__': 
 
     db = MySQLdb.connect(host='localhost',
                         port=3306,
-                        user=sys.argv[1],
-                        passwd=sys.argv[2],
-                        db=sys.argv[3])
+                        user=argv[1],
+                        passwd=argv[2],
+                        db=argv[3])
 
     cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY states.id ASC')
