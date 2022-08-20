ab4944e37f49b70102ac58895923ba4542d7bd69
JSM788 <salinasjuan788@gmail.com>
2022-08-19 23:34:37 -0500

    Fixing identation

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-"""This script takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response"""
+"""This script takes in a URL and an email, sends a POST request\
+to the passed URL with the email as a parameter, and displays the\
+body of the response."""
 if __name__ == "__main__":
-        import sys
-        import urllib.request
-        import urllib.parse
+    import sys
+    import urllib.request
+    import urllib.parse
 
-        url = sys.argv[1]
-        email = sys.argv[2]
-        param = {"email": email}
-        data = urllib.parse.urlencode(param)
-        data = data.encode("ascii")
-        req = urllib.request.Request(url, data)
-        with urllib.request.urlopen(req) as response:
-                print(response.read().decode("utf-8"))
+    url = sys.argv[1]
+    email = sys.argv[2]
+    param = {"email": email}
+    data = urllib.parse.urlencode(param)
+    data = data.encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode("utf-8"))
