dd11bd60ca2ee87451b09edd37c00f2505486d3d
JSM788 <salinasjuan788@gmail.com>
2022-09-13 14:47:25 -0500

    Write a script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  if (!error) {
+    console.log(data);
+  } else {
+    console.log(error);
+  }
+});
