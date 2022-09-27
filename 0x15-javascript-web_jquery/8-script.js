8db95f44fdf2875ba77f6df82f751d30ccc0188c
JSM788 <salinasjuan788@gmail.com>
2022-09-26 23:14:32 -0500

    Write a JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,6 @@
+$.getJSON('https://swapi-api.hbtn.io/api/films/?format=json',
+  function (data) {
+    $.each(data.results, (index, element) => {
+      $('UL#list_movies').append('<li>' + element.title + '</li>');
+    });
+  });
